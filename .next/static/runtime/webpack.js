c9d96459e6261c8f698dbf841eb29b962002a74d
jledezma33 <jledezma@student.neumont.edu>
2019-05-20 13:41:13 -0600

    Asking me to commit before I pull

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "80dbb2009eebfa6b45c5";
+/******/ 	var hotCurrentHash = "b4777a385552e1c41420";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
